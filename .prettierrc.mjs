6bf973ca3a8a57d0c252a6754732bfc4deac009a
0xx32 <dimas1523@mail.ru>
2025-03-13 19:16:27 +0300

    init

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+export default {
+	trailingComma: 'es5',
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: false,
+	bracketSpacing: true,
+	useTabs: true,
+	printWidth: 100,
+	endOfLine: 'lf',
+}
