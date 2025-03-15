2c58a0bba55708c168f1711bae5733e813b52248
0xx32 <dimas1523@mail.ru>
2025-03-15 14:09:30 +0300

    fix: auth

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,6 +7,7 @@ import { Providers } from './providers'
 import { getProfile } from './utils/api/requests'
 import { AUTH_LOCAL_KEY } from './utils/constants/global'
 import { queryClient } from './utils/globals/queryClient'
+import { router } from './utils/globals/router'
 
 import 'virtual:uno.css'
 import '@unocss/reset/tailwind.css'
@@ -19,17 +20,31 @@ const providersProps: Omit<ProvidersProps, 'children'> = {
 	queryClient,
 }
 
+
+
 async function init() {
 	const token = localStorage.getItem(AUTH_LOCAL_KEY)
 
-	if (token) {
-		const getProfileQuery = await queryClient.fetchQuery({
-			queryKey: ['getProfile'],
-			queryFn: () => getProfile({}),
-		})
 
-		providersProps.profile.defaultProfile = getProfileQuery.profile
-		providersProps.session.defaultSession = !!getProfileQuery
+
+	if (token) {
+
+		try {
+			const getProfileQuery = await queryClient.fetchQuery({
+				queryKey: ['getProfile'],
+				queryFn: () => getProfile({}),
+			})
+
+			providersProps.profile.defaultProfile = getProfileQuery.profile
+			providersProps.session.defaultSession = !!getProfileQuery
+		} catch (error) {
+			console.error(error);
+			
+			router.history.push(window.location.search)
+		}
+
+
+
 
 		createRoot(document.getElementById('root')!).render(
 			<Providers {...providersProps}>
