979d4eb08afca28116eca552fcd488d73d373c0d
0xx32 <dimas1523@mail.ru>
2025-03-20 23:40:22 +0300

    update styles

--- apps/web/src/routes/(base)/_base.tsx
+++ apps/web/src/routes/(base)/_base.tsx
@@ -23,12 +23,19 @@ export const Route = createFileRoute('/(base)/_base')({
 function LayoutComponent() {
 	return (
 		<div className="container py-10">
-			<div className="border border-gray-200 rounded-board bg-dashboard px-12 pt-6 shadow-xl dark:border-gray-900 dark:shadow-md dark:shadow-white/20">
-				<div className="grid grid-cols-[240px_1fr_270px] mx-auto max-w-[1400px] gap-5">
+			<div className="border border-gray-200 rounded-board bg-dashboard px-8 pt-6 shadow-xl dark:border-gray-900 dark:shadow-md dark:shadow-white/20">
+				<div className="grid grid-cols-[240px_1fr_270px] mx-auto max-w-[1400px] gap-10">
 					<Sidebar />
 					<main>
 						<Outlet />
 					</main>
+
+					<div>
+						<div className="sticky top-5">
+							<div className="mb-6 h-[300px] rounded-2xl bg-board"></div>
+							<div className="h-[300px] rounded-2xl bg-board"></div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
